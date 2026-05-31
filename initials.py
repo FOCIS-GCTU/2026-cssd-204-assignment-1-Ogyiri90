1f193c462e8c0852a24e02d36db23e8632bccafb
Ogyiri90 <2425402808@live.gctu.edu.gh>
2026-05-31 18:06:20 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Prints stylized large block letters for the initials O.E.A.
+# Assignment Number: 1
+#
+# Name: Ogyiri Emmanuel Amo
+# STUDENT ID: 2425402808
+# Email: 2425402808@live.gctu.edu.gh
+# Grader: Carolyn
+#
+# On my honor, Ogyiri Emmanuel Amo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Prints a stylized 10-line high block letter display of the initials O.E.A.
+    print()
+    print("...OEA")
+    print()
+    print("......OOOOOOOO......EEEEEEEEEEEE......AAAAAAAA....")
+    print("....OOOOOOOOOOOO....EEEEEEEEEEEE....AAAAAAAAAAAA..")
+    print("...OOOO......OOOO...EE..............AA........AA..")
+    print("...OO..........OO...EE..............AA........AA..")
+    print("...OO..........OO...EEEEEEEEEE......AAAAAAAAAAAA..")
+    print("...OO..........OO...EEEEEEEEEE......AAAAAAAAAAAA..")
+    print("...OO..........OO...EE..............AA........AA..")
+    print("...OOOO......OOOO...EE..............AA........AA..**")
+    print("....OOOOOOOOOOOO....EEEEEEEEEEEE....AA........AA..**")
+    print("......OOOOOOOO......EEEEEEEEEEEE....AA........AA....")
+    print()
 
 
 main()
